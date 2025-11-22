26db114c1480e164c23c9f72a6e736fa87ef50e5
zhangjiahao <zhangjiahao@dingliguangdang.com>
2025-11-22 17:26:27 +0800

    router registry和distributed在同一个进程会nacosMcpOperationServiceMap.get(name)为null的异常问题

--- auto-configurations/spring-ai-alibaba-autoconfigure-mcp-distributed/src/main/java/com/alibaba/cloud/ai/autoconfigure/mcp/discovery/client/NacosMcpAutoConfiguration.java
+++ auto-configurations/spring-ai-alibaba-autoconfigure-mcp-distributed/src/main/java/com/alibaba/cloud/ai/autoconfigure/mcp/discovery/client/NacosMcpAutoConfiguration.java
@@ -16,21 +16,21 @@
 
 package com.alibaba.cloud.ai.autoconfigure.mcp.discovery.client;
 
-import com.alibaba.cloud.ai.mcp.nacos.NacosMcpClientProperties;
-import com.alibaba.cloud.ai.mcp.nacos.service.NacosMcpOperationService;
-import com.alibaba.nacos.api.PropertyKeyConst;
-import com.alibaba.nacos.api.exception.NacosException;
-import com.alibaba.nacos.api.utils.StringUtils;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Properties;
 
 import static com.alibaba.cloud.ai.mcp.nacos.NacosMcpProperties.DEFAULT_ADDRESS;
+import com.alibaba.cloud.ai.mcp.nacos.NacosMcpClientProperties;
+import com.alibaba.cloud.ai.mcp.nacos.service.NacosMcpOperationService;
+import com.alibaba.nacos.api.PropertyKeyConst;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.utils.StringUtils;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author yingzi
@@ -41,7 +41,7 @@ import static com.alibaba.cloud.ai.mcp.nacos.NacosMcpProperties.DEFAULT_ADDRESS;
 public class NacosMcpAutoConfiguration {
 
 	@Bean
-	public Map<String, NacosMcpOperationService> nacosMcpOperationServiceMap(NacosMcpClientProperties nacosMcpClientProperties) {
+    public Map<String, NacosMcpOperationService> nacosMcpOperationServiceMap(NacosMcpClientProperties nacosMcpClientProperties, ObjectProvider<NacosMcpOperationService> registryServiceProvider) {
 		Map<String, NacosMcpOperationService> map = new HashMap<>();
 		nacosMcpClientProperties.getConfigs().forEach((name, nacosSseParameters) -> {
 			Properties properties = new Properties();
@@ -60,11 +60,9 @@ public class NacosMcpAutoConfiguration {
 			else {
 				properties.put(PropertyKeyConst.ENDPOINT, endpoint);
 			}
-
 			if (StringUtils.isEmpty(nacosSseParameters.serverAddr()) && StringUtils.isEmpty(nacosSseParameters.endpoint())) {
 				properties.put(PropertyKeyConst.SERVER_ADDR, DEFAULT_ADDRESS);
 			}
-
 			try {
 				NacosMcpOperationService nacosMcpOperationService = new NacosMcpOperationService(properties);
 				map.put(name, nacosMcpOperationService);
@@ -72,7 +70,9 @@ public class NacosMcpAutoConfiguration {
 				throw new RuntimeException(e);
 			}
 		});
-
+        registryServiceProvider.ifAvailable(service -> {
+            map.put("nacosMcpOperationService", service);
+        });
 		return map;
 	}
 
--- auto-configurations/spring-ai-alibaba-autoconfigure-mcp-distributed/src/main/java/com/alibaba/cloud/ai/autoconfigure/mcp/discovery/client/NacosMcpSseClientAutoConfiguration.java
+++ auto-configurations/spring-ai-alibaba-autoconfigure-mcp-distributed/src/main/java/com/alibaba/cloud/ai/autoconfigure/mcp/discovery/client/NacosMcpSseClientAutoConfiguration.java
@@ -16,29 +16,28 @@
 
 package com.alibaba.cloud.ai.autoconfigure.mcp.discovery.client;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import com.alibaba.cloud.ai.mcp.discovery.client.transport.DistributedAsyncMcpClient;
 import com.alibaba.cloud.ai.mcp.discovery.client.transport.DistributedSyncMcpClient;
 import com.alibaba.cloud.ai.mcp.discovery.client.transport.sse.SseWebFluxDistributedAsyncMcpClient;
 import com.alibaba.cloud.ai.mcp.discovery.client.transport.sse.SseWebFluxDistributedSyncMcpClient;
 import com.alibaba.cloud.ai.mcp.nacos.NacosMcpClientProperties;
-import com.alibaba.cloud.ai.mcp.nacos.service.NacosMcpOperationService;
 import com.alibaba.cloud.ai.mcp.nacos.NacosMcpSseClientProperties;
-import org.springframework.beans.factory.ObjectProvider;
+import com.alibaba.cloud.ai.mcp.nacos.service.NacosMcpOperationService;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 /**
  * @author yingzi
  * @since 2025/10/28
  */
-@AutoConfiguration(after = { NacosMcpAutoConfiguration.class })
+@AutoConfiguration
 @EnableConfigurationProperties({ NacosMcpSseClientProperties.class, NacosMcpClientProperties.class})
 @ConditionalOnProperty(prefix = "spring.ai.alibaba.mcp.nacos.client", name = { "enabled" }, havingValue = "true",
         matchIfMissing = false)
@@ -48,11 +47,9 @@ public class NacosMcpSseClientAutoConfiguration {
     @ConditionalOnProperty(prefix = "spring.ai.mcp.client", name = { "type" }, havingValue = "SYNC",
             matchIfMissing = true)
     public List<DistributedSyncMcpClient> sseWebFluxDistributedSyncClients(
-            ObjectProvider<Map<String, NacosMcpOperationService>> nacosMcpOperationServiceMapProvider,
+            Map<String, NacosMcpOperationService> nacosMcpOperationServiceMap,
             NacosMcpSseClientProperties nacosMcpSseClientProperties, ApplicationContext applicationContext) {
-        Map<String, NacosMcpOperationService> nacosMcpOperationServiceMap = nacosMcpOperationServiceMapProvider.getObject();
         List<DistributedSyncMcpClient> clients = new ArrayList<>();
-
         nacosMcpSseClientProperties.getConnections().forEach((name, nacosSseParameters) -> {
             SseWebFluxDistributedSyncMcpClient client = SseWebFluxDistributedSyncMcpClient.builder()
                     .serverName(nacosSseParameters.serviceName())
@@ -63,7 +60,6 @@ public class NacosMcpSseClientAutoConfiguration {
             client.init();
             client.subscribe();
             clients.add(client);
-
         });
         return clients;
     }
@@ -72,11 +68,9 @@ public class NacosMcpSseClientAutoConfiguration {
     @ConditionalOnProperty(prefix = "spring.ai.mcp.client", name = { "type" }, havingValue = "ASYNC",
             matchIfMissing = true)
     public List<DistributedAsyncMcpClient> sseWebFluxDistributedAsyncClients(
-            ObjectProvider<Map<String, NacosMcpOperationService>> nacosMcpOperationServiceMapProvider,
+            Map<String, NacosMcpOperationService> nacosMcpOperationServiceMap,
             NacosMcpSseClientProperties nacosMcpSseClientProperties, ApplicationContext applicationContext) {
-        Map<String, NacosMcpOperationService> nacosMcpOperationServiceMap = nacosMcpOperationServiceMapProvider.getObject();
         List<DistributedAsyncMcpClient> clients = new ArrayList<>();
-
         nacosMcpSseClientProperties.getConnections().forEach((name, nacosSseParameters) -> {
             SseWebFluxDistributedAsyncMcpClient client = SseWebFluxDistributedAsyncMcpClient.builder()
                     .serverName(nacosSseParameters.serviceName())
@@ -86,7 +80,6 @@ public class NacosMcpSseClientAutoConfiguration {
                     .build();
             client.init();
             client.subscribe();
-
             clients.add(client);
         });
         return clients;
